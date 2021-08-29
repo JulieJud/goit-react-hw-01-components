3d618a6d087d03ceea97397dbd64e8ae19abfc21
Julie Judachina <iuliia.saunova@email.cz>
2021-08-29 14:37:01 +0200

    add style to profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,14 @@ import 'App.css';
 import SocialCard  from './SosialCard/SocialCard';
 import user from '../date/user.json'
 
- const App = () => {
+function App () {
   return <div>
-     <SocialCard
-        avatar={user.avatar}
+    { /*<SocialCard user={user} />*/}
+    <SocialCard avatar={user.avatar}
         name={user.name}
         tag={user.tag}
         location={user.location}
-        stats={user.stats}
-      />
+        stats={user.stats}/>
   </div>;
 };
 
