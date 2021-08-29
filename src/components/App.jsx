1c59dcdca02e4762205c19a1094cdeba0628a2a5
Julie Judachina <iuliia.saunova@email.cz>
2021-08-29 16:19:58 +0200

    done statistic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
 
 
-import SocialCard  from './SosialCard/SocialCard';
+import SocialCard from './SosialCard/SocialCard';
+import Statistic from './Statistic/Statistic';
+
 import user from '../upcomingDate/user.json'
+import statisticData from '../upcomingDate/statisticData.json'
 
 function App () {
   return <div>
-    { /*<SocialCard user={user} />*/}
+   
     <SocialCard avatar={user.avatar}
         name={user.name}
         tag={user.tag}
         location={user.location}
-        stats={user.stats}/>
+      stats={user.stats} />
+    
+   <Statistic title="Upload stats" stats={statisticData} />;
+
   </div>;
 };
 
--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -3,29 +3,38 @@ import css from './Statistic.module.css';
 
 import PropTypes from 'prop-types';
 
-function Statistic({ }) {
+function Statistic({ title, stats }) {
+    function randomColor() {
+    return `rgb(
+    ${Math.floor(Math.random() * 256)},
+    ${Math.floor(Math.random() * 256)}, 
+    ${Math.floor(Math.random() * 256)})
+    `;
+  }
     return (
-        <section class="statistics">
-  <h2 class="title">Upload stats</h2>
+        <section className={css.statistics}>
+           {title && <h2 className={css.title}>{title}</h2>}
 
-  <ul class="stat-list">
-    <li class="item">
-      <span class="label">.docx</span>
-      <span class="percentage">4%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp3</span>
-      <span class="percentage">14%</span>
-    </li>
-    <li class="item">
-      <span class="label">.pdf</span>
-      <span class="percentage">41%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp4</span>
-      <span class="percentage">12%</span>
-    </li>
-  </ul>
-</section>
-    )
+             <ul className={css.statList}>
+        {stats.map(stat => (
+          <li className={css.item} key={stat.id} style={{ backgroundColor: randomColor() }}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage} %</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
+
+Statistic.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    }),
+  ),
+};
+export default Statistic
